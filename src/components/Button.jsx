5b9c44fe400f3058eab9c0e805d84a6bcecdee0d
ChaniceIrungu <chanicewanjiku@gmail.com>
2025-01-30 18:11:06 +0300

    updates to improve performance

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,23 +1,19 @@
-import React from "react";
 import "../styles/Button.css";
-import { handleClick } from "./eventHandlers.jsx";
+// import { handleClick } from "./eventHandlers.jsx";
 
 const Button = () => {
   const url = "https://store.steampowered.com/app/3205090/Jumping_Jack/";
   
   return (
-    <button 
-      className="steam-button" 
-      onClick={(e) => handleClick(e, url)}
-    >
-      <img 
-        className="board-image" 
-        src="/Images/board-fotor.png" 
-        alt="Steam Board"
-      />
-      <h4 className="b-text1">AVAILABLE FOR FREE!!!</h4>
-      <h2 className="b-text2">Play now on STEAM®</h2>
-    </button>
+    <a href={url} target="_blank" rel="noopener noreferrer">
+      <button className="steam-button">
+        <img 
+          className="board-image w-48 md:w-64 h-auto"
+          src="/Images/board-fotor.webp"
+          alt="Steam Board"
+        />
+      </button>
+    </a>
   );
 };
 
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
+  build: {
+    minify: "esbuild", // Use esbuild (faster) or "terser" (slower but smaller)
+    terserOptions: {
+      compress: {
+        drop_console: true, // Remove console logs
+        drop_debugger: true, // Remove debuggers
+      },
+      output: {
+        comments: false, // Remove comments
+      },
+    },
+  },
 })
